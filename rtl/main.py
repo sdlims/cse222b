01bcf27123584590f9754c27acd308bff7864c59
Sean Li <theseanisalie@gmail.com>
2025-05-22 13:36:27 -0700

    Most Updated for DR

--- rtl/main.py
+++ rtl/main.py
@@ -1,4 +1,5 @@
 import zlib
+import bz2
 import binascii
 import pprint
 import sys
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 prep_data = []
-compression_result = {}
+comp_data = []
 
 def preprocess_data(file_i):
     init_data = []
@@ -42,6 +43,9 @@ def preprocess_data(file_i):
             continue
         prep_data.append(line)
 
+        idx = line.find(" ")
+        comp_data.append(line[0:idx])
+
     #print("\n".join(prep_data))
     #breakpoint()
 
@@ -51,47 +55,42 @@ def get_subckt(indices):
         lineA += prep_data[i]
     return lineA
 
+def get_subgate(indices):
+    comp = []
+    for i in range(len(indices)):
+        comp.append(comp_data[indices[i]])
+    return comp
 
 def regularity_extraction(A, B):
     lineA = get_subckt(A)
     lineB = get_subckt(B)
 
-    # print(lineA, "\n")
-    # print(lineB, "\n")
+    gateA = get_subgate(A)
+    gateB = get_subgate(B)
 
-    tplt_avg = (len(A) + len(B)) / 2
-
-    size_penalty = 0
-    template_incentive = 0
+    if (gateA == gateB):
+        template_identicality = 1.1
+    else:
+        template_identicality = 0.9
 
     # Punishes Having Different Sized Templates
     if len(A) > len(B):
-        size_penalty = len(B) / (len(A) + len(B))
+        size_adjust = (len(B)) / (len(A))
     elif (len(B) > len(A)):
-        size_penalty = len(A) / (len(A) + len(B))
+        size_adjust = (len(A)) / (len(B))
     else:
-        size_penalty = 1
-    
-    if (tplt_avg >= 7):
-        template_incentive = 1.5
-    elif (tplt_avg >= 4):
-        template_incentive = 1.3
-    elif (tplt_avg == 1):
-        template_incentive = 0.01
-    else:
-        template_incentive = 1
- 
-    cmprA = zlib.compress(lineA.encode())
-    cmprB = zlib.compress(lineB.encode())
+        size_adjust = 1.5
 
     lineAB = lineA + lineB
-    cmprAB = zlib.compress(lineAB.encode())
+    cmprAB = bz2.compress(lineAB.encode())
 
     cmpr_out = len(lineAB) / len(cmprAB)
 
-    # print("Compression Out:", cmpr_out)
+    # print("Compression Ratio: ", cmpr_out)
+    # print("Size Adjustments: ", size_adjust)
+    # print("Template Average Size: ", tplt_avg, "\n")
 
-    return (size_penalty * cmpr_out * template_incentive) / tplt_avg
+    return (size_adjust * cmpr_out * template_identicality) 
 
 
 # For some vector V, get the complete compression ratio
@@ -106,16 +105,21 @@ def compression_annealing(V):
 
     # print("Subgraph: ", subgraph)
 
-    compression_eq = 100
+    compression_eq = 0
+    cmpr_cnt = 0
     for i in range(1, len(subgraph)):
         for j in range(i+1, len(subgraph)):
+            cmpr_cnt += 1
             if (len(subgraph[i]) == 0) or (len(subgraph[j]) == 0):
                 continue
             else:
                 # print("Comparing ", subgraph[i], "and", subgraph[j])
-                compression_eq *= regularity_extraction(subgraph[i], subgraph[j])
+                compression_eq += regularity_extraction(subgraph[i], subgraph[j])
+                # print("Compression Out: ", compression_eq, "\n")
 
-    return (compression_eq / len(subgraph)) #Averages
+    # print("Compression Out: ", compression_eq)
+    # print("# of Subgraphs: ", len(subgraph) - 1, "\n")
+    return (compression_eq / cmpr_cnt) if cmpr_cnt else 0 #Averages
     
 def compression_comparison(U, V):
     cmpr_U = compression_annealing(U)
@@ -141,7 +145,7 @@ def simulated_annealing(init_V, cooling_rate, start_temp, end_temp, max_iters, c
         neighbor_cmpr = compression_annealing(neighbor_V)
 
         delta_energy = neighbor_cmpr - current_cmpr
-        if (delta_energy > 0.01) or (random.random() < math.exp(delta_energy / temp)):
+        if (delta_energy > 0) or (random.random() < math.exp(delta_energy / temp)):
             current_V = neighbor_V
             current_cmpr = neighbor_cmpr
 
@@ -170,55 +174,46 @@ def simulated_change(V, cutoff):
     low_b = 0
 
     idx = random.randint(0, upp_b - 1)
-    change = random.randint(-1, 1)
 
     chance = random.randint(1, 100)
+    if (chance % 2 == 0):
+        change = -1
+    else:
+        change = 1
 
     copy_V = V[:]
 
     cutoff = cutoff * 100
 
-    if (copy_V[idx] == (upp_b / 2)):
+    if (copy_V[idx] >= (upp_b//2)):
         if (chance <= cutoff):
             # print("Swap!")
-            intermediate = copy_V[idx]
-            swap_idx = random.randint(0, upp_b - 1)
+            swap_idx = random.randint(0, len(V) - 1)
+            copy_V[idx], copy_V[swap_idx] = copy_V[swap_idx], copy_V[idx]
 
-            if (copy_V[idx] == copy_V[swap_idx]):
-                swap_idx = random.randint(0, upp_b - 1)
-
-            copy_V[idx] = copy_V[swap_idx]
-            copy_V[swap_idx] = intermediate
         else:
             # print("Change!")
             copy_V[idx] -= 1
+
     elif (copy_V[idx] == low_b):
         if (chance <= cutoff):
             # print("Swap!")
-            intermediate = copy_V[idx]
-            swap_idx = random.randint(0, upp_b - 1)
-            copy_V[idx] = copy_V[swap_idx]
-            copy_V[swap_idx] = intermediate
+            swap_idx = random.randint(0, len(V) - 1)
+            copy_V[idx], copy_V[swap_idx] = copy_V[swap_idx], copy_V[idx]
+
         else:
             # print("Change!")
             copy_V[idx] += 1
+
     else:
         if (chance <= cutoff):
             # print("Swap!")
-            intermediate = copy_V[idx]
-            swap_idx = random.randint(0, upp_b - 1)
+            swap_idx = random.randint(0, len(V) - 1)
+            copy_V[idx], copy_V[swap_idx] = copy_V[swap_idx], copy_V[idx]
 
-            if (copy_V[idx] == copy_V[swap_idx]):
-                swap_idx = random.randint(0, upp_b - 1)
-                
-            copy_V[idx] = copy_V[swap_idx]
-            copy_V[swap_idx] = intermediate
         else:
             # print("Change!")
-            if (copy_V[idx] >= (upp_b // 2)):
-                copy_V[idx] -= 1
-            else:
-                copy_V[idx] += change
+            copy_V[idx] += change
 
     return copy_V
 
@@ -235,17 +230,28 @@ def main():
     comparisons = []
     preprocess_data(input_file)
 
-
-    #pprint.pprint(simulated_annealing([1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 3, 3, 3, 3, 3, 4, 4, 4, 4, 2], 0.9999, 100000, 1, 1000000, 1))
+    #[1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 3, 3, 3, 3, 3, 4, 4, 4, 4, 2]
+    init_V = [random.randint(0, len(prep_data)//4) for _ in range(len(prep_data))]
+    random.shuffle(init_V)
+    pprint.pprint(simulated_annealing([1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 1, 1, 1, 1, 2], 0.98, 10, 0.01, 5000, 0.8))
     
 
-    print("Garbage:")
-    print(compression_annealing([4, 2, 2, 4, 1, 2, 3, 3, 2, 1, 2, 1, 3, 3, 1, 4, 4, 4, 3, 1]))
-    print("\n")
+    # print("Garbage:")
+    # print(compression_annealing([1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 3, 3, 3, 3, 3, 4, 4, 4, 4, 2]))
+    # print("\n")
 
-    print("Group each Ripple Adder:")
-    print(compression_annealing([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4]))
-    print("\n")
+    # print("Garbage:")
+    # print(compression_annealing([1, 2, 3, 1, 4, 3, 4, 4, 3, 4, 1, 2, 2, 1, 3, 3, 2, 2, 1, 4]))
+    # print("\n")
+
+    # print("Garbage:")
+    # print(compression_annealing([3, 3, 3, 3, 2, 4, 3, 1, 4, 4, 2, 2, 2, 2, 1, 1, 4, 4, 1, 1]))
+    # print("\n")
+    
+
+    # print("Group each Ripple Adder:")
+    # print(compression_annealing([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4]))
+    # print("\n")
 
     # print("Group By Output:")
     # print(compression_annealing([1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 8]))
