2861e864b79243819a0ed63d10f1ecd3e30fd536
Sean Li <theseanisalie@gmail.com>
2025-06-02 17:46:31 -0700

    Updated RE with Gate/Netlist method

--- rtl/regularity_extraction.py
+++ rtl/regularity_extraction.py
@@ -53,7 +53,32 @@ def get_subgate(indices):
     comp = []
     for i in range(len(indices)):
         comp.append(comp_data[indices[i]])
+    comp.sort() # NEW LINE
     return comp
+
+
+def nets_parse(data):
+    start_idx = data.index("(")
+    stop_idx = data.index(")")
+    data = data[start_idx+1:stop_idx]
+    data = data.split(",")
+    data = [item.strip() for item in data]
+
+    return data
+
+
+def get_nets(indices):
+    nets = []
+    for i in indices:
+        net_data = nets_parse(prep_data[i])
+
+        max_len = max(net_data, key=len)
+        norm_net_data = [item.zfill(len(max_len)) for item in net_data]
+        nets.append(norm_net_data)
+    
+    return nets
+    # print(nets)
+
 
 
 def size_re(lenA, lenB):
@@ -64,6 +89,7 @@ def size_re(lenA, lenB):
             return (lenA / lenB)
     else:
         return 1
+
 
 def gate_re(gateA, gateB):
     if (gateA == gateB):
@@ -86,6 +112,7 @@ def gate_re(gateA, gateB):
                 j += 1
         return re
 
+
 def regularity_extraction(A, B):
     lineA = get_subckt(A)
     lineB = get_subckt(B)
@@ -93,8 +120,18 @@ def regularity_extraction(A, B):
     gateA = get_subgate(A)
     gateB = get_subgate(B)
 
-    # pprint.pprint(lineA)
+    netA = get_nets(A)
+    netB = get_nets(B)
+    
+    netAstr = ''.join(str(i) for i in netA)
+    netBstr = ''.join(str(i) for i in netB)
 
+    cmprAnet = bz2.compress(netAstr.encode())
+    cmprBnet = bz2.compress(netBstr.encode())
+
+    netABstr = netAstr + netBstr
+    cmprABnet = bz2.compress(netABstr.encode())
+    
     cmpr_gate = gate_re(gateA, gateB)
 
     cmprA = bz2.compress(lineA.encode())
@@ -105,6 +142,7 @@ def regularity_extraction(A, B):
 
     cmpr_main = (len(cmprAB) / (len(cmprA) + len(cmprB)))
 
-    cmpr_size = size_re(len(A), len(B))
+    cmpr_net_main = (len(cmprABnet) / (len(cmprAnet) + len(cmprBnet)))
 
-    return cmpr_main * cmpr_size * cmpr_gate * scalar # Scalar normalizes answers [0:1]
+
+    return cmpr_main * cmpr_gate * cmpr_net_main * scalar # Scalar normalizes answers [0:1]
